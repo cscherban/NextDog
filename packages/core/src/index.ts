5f3b288639949c29af8028e6284e93f1616073d1
Scherban <christopher.scherban@gmail.com>
2026-03-21 15:23:58 -0400

    scaffold monorepo with pnpm workspaces and turborepo

---
+++ packages/core/src/index.ts
@@ -0,0 +1,6 @@
+export { EventBus } from './event-bus.js';
+export { RingBuffer } from './ring-buffer.js';
+export { FileStore } from './file-store.js';
+export { SSEStream } from './sse-stream.js';
+export { createServer } from './server.js';
+export type { Span, LogEntry, NextDogEvent } from './types.js';
